0d3b86ef9a5f2db7b96b10cfada94e2ba96fd531
nikun <nikun2008@126.com>
2023-11-15 12:02:48 +0800

    update

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,46 @@
 "use client";
 
-export default function Index() {
+export default function About() {
   return (
-    <div
-      className={
-        "bg-red-100 flex min-h-screen flex-col items-center justify-between p-24"
-      }
-    >
-      About
-    </div>
+    <article className={"container flex min-h-screen flex-col "}>
+      <header>
+        <h2 className={"text-3xl text-center"}>About Us</h2>
+      </header>
+      <p className={"indent-8 mt-2 text-lg text-gray-600"}>
+        We know what it means to want to get something done so bad but you
+        can&apos;t because there is a restriction on your ID. Yeah. We exist to
+        solve this problem. We know what it means to want to get something done
+        so bad but you can&apos;t because there is a restriction on your ID.
+        Yeah. We exist to solve this problem.
+      </p>
+      <p className={"indent-8 mt-2 text-lg text-gray-600"}>
+        Usa fakeid offers original fake ID cards at affordable prices. (The
+        irony of &apos;original fake&apos; ID? Yeah!) Either you need a
+        State&apos;s ID or a Driver license, we will provide you with fake ID
+        cards that would reflect under UV light and other light testing systems.
+        Our IDs have the correct OVI holograms that produce the right
+        reflection, details, and shadows under UV blacklight, laser perforation,
+        and so on. We take our (prospective) customers seriously and pride in
+        prompt response to messages. We respond to all queries within 12 hours,
+        depending on your time zone.
+      </p>
+      <p className={"indent-8 mt-2 text-lg text-gray-600"}>
+        With almost a decade of experience in making Identity cards, the team
+        members of usafakeid offers premium services. We have the right tools to
+        implement every effect that is expected for an ID card. Knowing how
+        discrete people want to be, we offer swift anonymous payment methods.
+        After the order is completed, nothing ever happened between us.
+      </p>
+      <p className={"indent-8 mt-2 text-lg text-gray-600"}>
+        If you have any questions, please contact:
+        <a
+          href="#"
+          className="text-blue-600 hover:underline dark:text-blue-400"
+          target="_blank"
+        >
+          order@usafakeid.com
+        </a>
+      </p>
+    </article>
   );
 }
--- src/app/faq/page.jsx
+++ src/app/faq/page.jsx
@@ -73,22 +73,31 @@ export default function Faq() {
 
   return (
     <div className={"container flex min-h-screen flex-col"}>
-      <h6>Ready answers to your questions</h6>
-      <Accordion type="single" collapsible defaultValue={"item-1"}>
+      <h6 className={"my-8"}>Ready answers to your questions</h6>
+      <Accordion type="single" collapsible>
         {faqs.map((item, index) => {
           return (
             <>
               <AccordionItem
-                className={"border-b border-gray-300"}
+                className={"my-3"}
                 value={`item-${index + 1}`}
                 key={index}
               >
-                <AccordionTrigger className={"text-left"}>
+                <AccordionTrigger
+                  className={
+                    "w-full text-white leading-normal bg-[#45484A] p-4 rounded-lg text-left data-[state=open]:rounded-b-none data-[state=open]:border-solid data-[state=open]:border-b-2 data-[state=open]:border-red-600"
+                  }
+                >
                   {item.trigger}
                 </AccordionTrigger>
-                <AccordionContent>{item.content}</AccordionContent>
+                <AccordionContent
+                  className={
+                    "w-full bg-gray-100 leading-normal p-4 rounded-lg data-[state=open]:rounded-t-none"
+                  }
+                >
+                  {item.content}
+                </AccordionContent>
               </AccordionItem>
-              <Separator />
             </>
           );
         })}
