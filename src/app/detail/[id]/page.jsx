7acc81f4a0af51890209270fd97219a0bee6a003
nikun <nikun2008@126.com>
2023-11-22 09:15:23 +0800

    update

--- src/app/detail/[id]/page.jsx
+++ src/app/detail/[id]/page.jsx
@@ -1,49 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import Product1 from "@/images/products/1.png";
 import Product2 from "@/images/products/2.png";
 import Product3 from "@/images/products/3.png";
 import Product4 from "@/images/products/4.png";
 import Product5 from "@/images/products/5.png";
 
-const Detail = ({ products }) => {
-  const [index, setIndex] = useState(0);
-  const { image } = products;
-  return (
-    <div className={""}>
-      <div>
-        <div className="image-container">
-          <img src={image && image[index]} className="product-detail-image" />
-        </div>
-        <div className="small-images-container">
-          {image?.map((item, i) => (
-            <img
-              key={i}
-              src={item}
-              className={
-                i === index ? "small-image selected-image" : "small-image"
-              }
-              onMouseEnter={() => setIndex(i)}
-            />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+const Detail = () => {
+  return <div className={""}>Detail 待完善</div>;
 };
 
-export const getStaticProps = async ({ params }) => {
-  const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-  const productsQuery = '*[_type == "product"]';
-
-  // const product = await client.fetch(query);
-  // const products = await client.fetch(productsQuery);
-
-  return {
-    props: {
-      products: {
-        image: [Product1, Product2, Product4, Product3, Product5],
-      },
-    },
-  };
-};
 export default Detail;
