02b4eee153c7a516a73da02c4c17661a74e251ed
nikun <nikun2008@126.com>
2023-11-22 10:15:30 +0800

    fix:首页banner图不对

--- src/components/SectionHero/SectionHero.jsx
+++ src/components/SectionHero/SectionHero.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import backgroundLineSvg from "@/images/Moon.svg";
 import { Button } from "@/components/ui/button";
 import Next from "@/shared/NextPrev/Next";
 import Prev from "@/shared/NextPrev/Prev";
--- src/components/SectionHero/data.js
+++ src/components/SectionHero/data.js
@@ -1,10 +1,10 @@
-import getYourFakeID from "@/images/get-your-fake-ID.jpg";
-import provenExperience from "@/images/proven-experience.jpg";
-import privacyPolicy from "@/images/privacy-policy.jpg";
+import Heart from "@/images/heart.jpg";
+import Cheers from "@/images/cheers.jpg";
+import PexelsDanielDinu from "@/images/pexels-daniel-dinu.jpg";
 
 export const HERO_DATA = [
   {
-    image: provenExperience,
+    image: Cheers,
     heading: "Get Your Fake ID",
     subHeading:
       "Get Your Fake ID and without feeling bothered. We provide premium scannable fake ID cards that would work anywhere in the world.",
@@ -12,7 +12,7 @@ export const HERO_DATA = [
     btnLink: "/",
   },
   {
-    image: privacyPolicy,
+    image: PexelsDanielDinu,
     heading: "Proven Experience",
     subHeading:
       "We have almost 10 years’ experience in this field and we guaranty your safety as it regards transactions, identity, and ID means. We have proven track records, and know what works. ",
@@ -20,7 +20,7 @@ export const HERO_DATA = [
     btnLink: "/",
   },
   {
-    image: getYourFakeID,
+    image: Heart,
     heading: "Privacy Policy",
     subHeading:
       "We do not hold or withhold any information submitted to our site. All data are permanently deleted when the real IDs have been successfully shipped to the customer. Hence, we are not able to reproduce the information if you or anyone ask us for such data. ",
